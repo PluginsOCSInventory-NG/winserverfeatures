b751b68d8fc17146022fe621d778abd1ab8367af
Gilles Dubois <gilles.dubois@factorfx.com>
2020-03-10 08:51:27 +0100

    Prettify code

--- agent/Windows/winserverfeatures.ps1
+++ agent/Windows/winserverfeatures.ps1
@@ -1,12 +1,12 @@
-$features =  Get-WindowsFeature | Where Installed | select * 
+$features = Get-WindowsFeature | Where Installed | select * 
 
 foreach ($feature in $features){
 	$xml += "<WINSERVERFEATURES>`n"
-	$xml += "<NAME>"+$feature.Name+"</NAME>`n"
-	$xml += "<DISPLAYNAME>"+$feature.DisplayName+"</DISPLAYNAME>`n"
-	$xml += "<DESCRIPTION>"+$feature.Description+"</DESCRIPTION>`n"
-	$xml += "<INSTALLED>"+$feature.Installed+"</INSTALLED>`n"
-	$xml += "<SUBFEATURES>"+$feature.SubFeatures+"</SUBFEATURES>`n"
+	$xml += "<NAME>" + $feature.Name + "</NAME>`n"
+	$xml += "<DISPLAYNAME>" + $feature.DisplayName + "</DISPLAYNAME>`n"
+	$xml += "<DESCRIPTION>" + $feature.Description + "</DESCRIPTION>`n"
+	$xml += "<INSTALLED>" + $feature.Installed + "</INSTALLED>`n"
+	$xml += "<SUBFEATURES>" + $feature.SubFeatures + "</SUBFEATURES>`n"
 	$xml += "</WINSERVERFEATURES>`n"
 }
 
